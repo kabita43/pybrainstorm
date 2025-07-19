d0240e0b65a3b6721660e3d583eea469ea23eb70
Kate Ana <kateana@Kates-MacBook-Air.local>
2025-07-19 17:45:32 +0545

    tuple method

---
+++ chapter 4/02problem.py
@@ -0,0 +1,18 @@
+marks= []
+f1=input("Enter marks :")
+marks.append(f1)
+f2=input("Enter marks :")
+marks.append(f2)
+f3=input("Enter marks :")
+marks.append(f3)
+f4=input("Enter marks :")
+marks.append(f4)
+f5=input("Enter marks :")
+marks.append(f5)
+f6=input("Enter marks :")
+marks.append(f6)
+f7=input("Enter marks :")
+marks.append(f7)
+marks.sort()
+print(marks)
+# print(sorting)2
--- chapter 4/tuples_method.py
+++ chapter 4/tuples_method.py
@@ -1,2 +1,4 @@
 a= (1,False,"python",3.5)
 print(type(a))
+no= a.count
+
