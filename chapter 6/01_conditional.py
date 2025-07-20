99bb55f91c9bed5682826cbb2bc47852bc2ff057
Kate Ana <kateana@Kates-MacBook-Air.local>
2025-07-20 18:07:38 +0545

    condition

---
+++ chapter 6/01_conditional.py
@@ -0,0 +1,10 @@
+a= int(input("Enter your age :"))
+if(a>18):
+    print("your are above the age of concent")
+elif(a<0):
+    print("Your are enter invalid age ")
+
+else:
+    print("your are below the age of concent")
+
+     
