da0ebb26934d07d2adbed7d9e1c74f76a7ac44f8
Kate Ana <kabitastha519@gmail.com>
2025-07-23 10:39:35 +0545

    project 1 completed

--- project 1/main.py
+++ project 1/main.py
@@ -7,18 +7,24 @@ computer= -1
 youstr= input("enter your choice :")
 youDic= {"s":1,"w":-1,"g":0}
 you= youDic[youstr]
+reverseDict= { 1: "snake", -1: "water",0:"gun"}
+print(f"you choose {reverseDict[you]}\nComputer choose{reverseDict[computer]} ")
 
-if(computer==-1 and you ==1):
-    print("you win!")
-elif(computer==1 and you == -1):
-    print("computer win!")
-elif(computer==-1 and you ==0):
-    print("computer win!")
-elif(computer==0 and you ==0):
-    print("computer win!")
-elif(computer==-1 and you ==0):
-    print("computer win!")
-elif(computer==-1 and you ==0):
-    print("computer win!")
+if(computer== you):
+    print("its a Draw")
+else:
+            
+    if(computer==-1 and you ==1):
+        print("you win!")
+    elif(computer==1 and you == -1):
+        print("computer win!")
+    elif(computer==-1 and you ==0):
+        print("computer win!")
+    elif(computer==1 and you ==0):
+        print("computer win!")
+    elif(computer==0 and you ==1):
+        print("computer win!")
+    else:
+        print("Something went wrong")    
 
 
